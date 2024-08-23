2e64ea197ee04f94520ba8f9ecd29194b398cea5
GabryelFreitasDEV <gabryelfreitas04@gmail.com>
2024-08-23 20:25:50 -0300

    Subindo o projeto v0.0.1

---
+++ src/pages/home.tsx
@@ -0,0 +1,19 @@
+import '../styles/utility.css'
+
+function Home () {
+    return (
+        <>
+            <header className='container'>
+                <ul className='even-columns'>
+                    <li><a>Home</a></li>
+                    <li><a>Soluções</a></li>
+                    <li><a>Depoimentos</a></li>
+                    <li><a>Preços</a></li>
+                    <li><a>Contatos</a></li>
+                </ul>
+            </header>
+        </>
+    )
+}
+
+export default Home;
