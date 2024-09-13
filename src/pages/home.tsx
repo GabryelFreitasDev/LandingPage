c72b7f8535a5c67a4c61b3a36d80faffb09e2ee3
GabryelFreitasDev <gabryelfreitas04@gmail.com>
2024-09-13 18:33:00 -0300

    Começo segunda seção

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,7 +1,11 @@
 import HeroRectangleOne from "../assets/images/rectangleOne.png";
 import HeroRectangleTwo from "../assets/images/rectangleTwo.png";
 import Button from "../components/Button";
+import TurboCharger from "../assets/images/turbocharger.png";
+import FuelTech from "../assets/images/fueltech.png";
+import NanoPro from "../assets/images/nanopro.png";
 import "../styles/hero.css";
+import "../styles/solutions.css";
 
 import Header from './header';
 
@@ -26,6 +30,27 @@ function Home () {
                         <span className="desktop-only">
                             <Button text="Veja mais" secondary />
                         </span>
+                    </div>
+                </div>
+            </section>
+            <section id="solutions" className="container">
+                <h2>Peças de performance para seu projeto</h2>
+                <p>Inovação é com a gente! A <strong>TurboLag</strong> já conquistou diversos clientes, seja você mais um deles. Veja tudo que pode ganhar com nossos serviços.</p>
+                <div className="cards-container">
+                    <div className="card">
+                        <img src={TurboCharger} alt="TurboCharger" style={{scale: "1.3"}}/>
+                        <h3>Produto Vencedor</h3>
+                        <p>Ideia matadora, nosso time já ganhou diversos eventos de inovação com esse produto. Entre eles podemos citar o CityFarm da FAG e Startup Garage.</p>
+                    </div>
+                    <div className="card">
+                        <img src={FuelTech} alt="TurboCharger" style={{scale: "1.2"}}/>
+                        <h3>Produto Vencedor</h3>
+                        <p>Ideia matadora, nosso time já ganhou diversos eventos de inovação com esse produto. Entre eles podemos citar o CityFarm da FAG e Startup Garage.</p>
+                    </div>
+                    <div className="card">
+                        <img src={NanoPro} alt="TurboCharger" style={{scale: "1"}}/>
+                        <h3>Produto Vencedor</h3>
+                        <p>Ideia matadora, nosso time já ganhou diversos eventos de inovação com esse produto. Entre eles podemos citar o CityFarm da FAG e Startup Garage.</p>
                     </div>
                 </div>
             </section>
